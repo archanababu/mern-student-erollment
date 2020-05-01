3c617f3b4195c10823530477494269f74a04eb60
arcbabu <archana.masterseducation@gmail.com>
2020-04-30 22:33:42 -0500

    Establishing connection to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 
+//Connect to MongoDB database
+const mongoose = require('mongoose');
+
 //This configures, so we can have environment variables in dotenv file
 require('dotenv').config();
 
@@ -12,7 +15,20 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-//Start server on port: 5000
+/*MongoDB URI and start connection with the db uri
+    ATLAS_URI : Environment variable stored in dotenv file
+    Flags:
+        - set useNewUrlParser, useCreateIndex to TRUE to deal with MongoDB updates
+*/
+const uri =  process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+
+const connection = mongoose.connection;
+connection.once('open', ()=>{
+    console.log("MongoDB database connection established successfully")
+});
+
+//Starts server on port: 5000
 app.listen(port, ()=>{
     console.log(`Server is running on port: ${port}`);
 });
